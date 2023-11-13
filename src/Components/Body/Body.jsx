296054899b221198c2fe3fafc77cdd3eabfdd150
Potykun <105052944+Potykun@users.noreply.github.com>
2023-11-13 13:20:59 +0100

    try to fetch data from cite of recipes

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 function Body(props) {
-	return <div>this is body component</div>;
+	const [users, setUsers] = useState([]);
+
+	const fetchDataUsers = () => {
+		fetch()
+			// "https://api.spoonacular.com/recipes/716429/information?apiKey=b343e33843de486b805a2225d34b8b44&includeNutrition=true."
+			.then((response) => response.json())
+			.then((data) => setUsers(data));
+	};
+	useEffect(() => {
+		fetchDataUsers();
+	}, []);
+	console.log("users", users);
+	return (
+		<div>
+			{users ?? (
+				<ul>
+					<li>{users.sourceName}</li>
+				</ul>
+			)}
+		</div>
+	);
 }
 
 export default Body;
