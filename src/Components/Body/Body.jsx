8146dd9de8c0de7ef3438f5f121656696adc8c3a
Potykun <105052944+Potykun@users.noreply.github.com>
2023-11-28 20:10:06 +0100

    clean jsx from tilwind

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -35,13 +35,11 @@ export default function Body() {
 		}
 	};
 	return (
-		<div className="flex flex-col md:px-12 px-0 relative bg-background font-raleway items-center min-h-screen">
-			<h1 className="text-6xl font-bold text-active mt-20">Recipe Search</h1>
-			<h2 className="text-primary text-2xl font-light mt-5">
-				Search recipes from all over the world.
-			</h2>
+		<div className="">
+			<h1 className="">Recipe Search</h1>
+			{/* <h2 className="">Search recipes from all over the world.</h2> */}
 			<form
-				className="sm:mx-auto mt-20 md:max-w-4xl justify-center flex flex-col sm:w-full sm:flex"
+				className="form"
 				onSubmit={(e) => {
 					getRecipes();
 					e.preventDefault();
@@ -50,18 +48,18 @@ export default function Body() {
 			>
 				<input
 					type="text"
-					className="flex w-full rounded-lg px-5 py-3 text-base text-background font-semibold focus:outline-none focus:ring-2 focus:ring-active"
+					className="recipe"
 					placeholder="Enter a recipe"
 					onChange={(e) => {
 						setKeyword(e.target.value);
 						setResponse(null);
 					}}
 				/>
-				<div className="mt-5 flex sm:flex-row flex-col justify-start">
-					<div className="sm:w-1/3 w-full">
-						<label className="block text-primary text-sm">diet1</label>
+				<div className="diet">
+					<div className="">
+						<label className="">diet1</label>
 						<select
-							className="mt-1 flex w-full rounded-lg px-5 py-3 text-base text-background font-bold focus:outline-none"
+							className=""
 							onChange={(e) => setdiet1(e.target.value)}
 						>
 							{[
@@ -71,62 +69,68 @@ export default function Body() {
 								"ovo vegetarian",
 								"vegan",
 								"vegetarian",
-							].map((diet1) => {
-								return <option value={diet1}>{diet1}</option>;
+							].map((diet1, indx) => {
+								return (
+									<option
+										key={indx}
+										value={diet1}
+									>
+										{diet1}
+									</option>
+								);
 							})}
 						</select>
 					</div>
-					<div className="sm:ml-10 sm:w-1/3 w-full">
-						<label className="block text-primary text-sm">
-							Exclude Ingredients
-						</label>
+					<div className="Exclude">
+						<label className="">Exclude Ingredients</label>
 						<input
 							type="text"
-							className="mt-1 w-full rounded-lg px-5 py-3 text-base text-background font-bold focus:outline-none"
+							className=""
 							placeholder="cocunut"
 							onChange={(e) => setExclude(e.target.value)}
 						></input>
 					</div>
 				</div>
 				<button
-					className="mt-5 w-full rounded-lg px-5 py-3 bg-active text-base text-primary font-bold hover:text-active hover:bg-primary transition-colors duration-300 sm:px-10"
+					className="buttonSearch"
 					type="submit"
 				>
 					Search
 				</button>
 			</form>
 			{response && ( // Render only if response is not null
-				<div className="mt-10">
-					<div className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+				<div className="responce">
+					<div
+						className="cnt-item"
+						style={{
+							display: "grid",
+							gridTemplateColumns: "repeat(3, 1fr)",
+						}}
+					>
 						{response.map((recipe) => (
 							<div
 								key={recipe.id}
-								className="pt-6"
+								className="responce-item"
 							>
-								<div className="flow-root bg-light rounded-lg px-4 pb-8">
-									<div className="-mt-6">
-										<div className="flex items-center justify-center">
-											<span className="p-2">
+								<div className="">
+									<div className="">
+										<div className="">
+											<span className="">
 												<img
-													src={
-														`https://spoonacular.com/recipeImages/` +
-														recipe.image
-													}
-													className="w-full h-full rounded-lg"
+													src={recipe.image}
+													className=""
 													alt={recipe.id}
 												/>
 											</span>
 										</div>
-										<div className="text-center justify-center items-center">
-											<h3 className="mt-4 text-lg font-bold w-full break-words overflow-x-auto text-primary tracking-tight">
-												{recipe.title}
-											</h3>
-											<span className="mt-2 text-sm text-secondary block">
+										<div className="">
+											<h3 className="">{recipe.title}</h3>
+											<span className="">
 												Ready in {recipe.readyInMinutes} minutes -{" "}
 												{recipe.servings} Servings
 											</span>
 											<a
-												className="mt-4 text-sm text-active block"
+												className=""
 												href={recipe.sourceUrl}
 											>
 												Go to Recipe
