c8b8fb3ef99f722a669504f1d3bfb579627b9b38
Potykun <105052944+Potykun@users.noreply.github.com>
2023-12-12 18:13:41 +0100

    Updates

---
+++ precache-manifest.df0eeb335b2a04df3fc47d6e0593b579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380b4c7a693ae9f365fa8333983b823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e6c453f560bbc558d0",
+    "url": "/static/css/main.87d8a5c6.chunk.css"
+  },
+  {
+    "revision": "80f14bdff483b5ac50a2",
+    "url": "/static/js/2.b85e4e35.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/static/js/2.b85e4e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e6c453f560bbc558d0",
+    "url": "/static/js/main.500811ad.chunk.js"
+  },
+  {
+    "revision": "1d2716a1ec1cad02ada6",
+    "url": "/static/js/runtime-main.0d9b48d6.js"
+  }
+]);
